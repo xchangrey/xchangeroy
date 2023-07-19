45794694fd51226d52ab955185aad91545e5f1b9
xchangrey <geroychristian@gmail.com>
2023-07-19 19:06:03 +0800

    feat: create react app

---
+++ client/src/PostCreate.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const PostCreate = () => {
+  const [ title, setTitle ] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios.post('http://localhost:4000/posts', {
+      title
+    });
+
+    setTitle('');
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label htmlFor="title">Title</label>
+          <input 
+            name='title' 
+            type="text" 
+            className='form-control' 
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+        </div>
+        <button className='btn btn-primary'>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default PostCreate;
