c570c19bba5b5feefa5b7e295ffb07da18ce0d5f
xchangrey <geroychristian@gmail.com>
2023-07-19 19:54:13 +0800

    refactor: use query service to pull up posts

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,27 +1,11 @@
-import React, { useEffect, useState} from 'react';
-import axios from 'axios';
+import React from 'react';
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-  const fetchData = async () => {
-    const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-    setComments(res.data);
-  }
-  
-  useEffect(() => {
-    fetchData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[]);
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map(comment => {
     return <li key={comment.id}>{comment.content}</li>
   });
 
-  return (
-    <ul>
-      {renderedComments}
-    </ul>
-  );
+  return <ul>{renderedComments}</ul>
 };
 
 export default CommentList;
